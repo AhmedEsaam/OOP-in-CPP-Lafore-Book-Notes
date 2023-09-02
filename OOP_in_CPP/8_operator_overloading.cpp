699ede15ffdf81e8fca51fa31086504f7e11c3a5
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-09-02 05:01:37 +0300

    adding overloading subscript [] operator

--- OOP_in_CPP/8_operator_overloading.cpp
+++ OOP_in_CPP/8_operator_overloading.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstring>
-#include <stdlib.h>
+#include <stdlib.h>     // for exit() or #include <process.h>
 using namespace std;
 
 /* Here is where the exciting lands ☻ */
@@ -41,6 +41,7 @@ public:
 
 /* Binary Operators ....................................*/
 // + operator (to add distances in English format)
+// += operator (Arithmetic assign)
 // and < operaor (to compare two English Distances)
 class Distance
 {
@@ -54,16 +55,19 @@ public:
     {   }
     void get_dist()
     {
-        cout << "Enter feets: ";  cin >> feet;
+        cout << "Enter feets: ";    cin >> feet;
         cout << "Enter inches: ";   cin >> inches;
     }
     void show_dist() const
-    {   cout << feet << "\' " << inches << "\"" << endl; }
+    {   cout << feet << "\' " << inches << "\""; }
     
     Distance operator +(Distance) const;  
+    void operator +=(Distance);  
+
     bool operator <(Distance) const;      
 };
         
+
 Distance Distance::operator +(Distance d2) const        
 {
     // the operand in the leftside in (op1 + op2) is the one that the operator is a member of.
@@ -78,6 +82,17 @@ Distance Distance::operator +(Distance d2) const
     return Distance(f, i);
 }
 
+void Distance::operator +=(Distance d2)        
+{
+    feet += d2.feet;
+    inches += d2.inches;
+    if(inches >= 12.0)
+    {
+        inches -= 12.0;
+        feet++;
+    }
+}
+
 bool Distance::operator <(Distance d2) const
 {
     return ((feet * 12 + inches) < (d2.feet * 12 + inches));
@@ -86,6 +101,8 @@ bool Distance::operator <(Distance d2) const
 
 
 // + operator (to concatinate C-strings)
+// == operator (to comapre C-strings)
+// and [] subscript operator (for safe accessing of C-strings)
 class String
 {
 private:
@@ -96,11 +113,19 @@ public:
     {   strcpy(str, ""); }
     String(char s[])
     {   strcpy(str, s); }
+    void get_str()
+    {   
+        cin.ignore();
+        cin.get(str, SIZE); 
+    }
     void display() const
     {   cout << str; }
 
-    String operator +(String str2) const;
+    String operator +(String) const;
+    bool operator ==(String) const;
+    char& operator [](int);
 };
+
 
 String String::operator +(String s2) const
 {
@@ -112,9 +137,21 @@ String String::operator +(String s2) const
     }
     else
     {
-        cout << "String overflow."; 
+        cout << "String overflow."; exit(1); 
     }
     return temp;
+}
+
+bool String::operator ==(String s2) const
+{   
+    return (strcmp(str, s2.str) == 0);
+}
+
+char& String::operator [](int i)    // An obj[i] can be used as L-value or R-value, so return by reference.
+{
+    if(i < 0 || i >= SIZE)
+    {   cout << "\nIndex out of bounds"; exit(1); }     // safe array: dosn't allow reading or inserting out of array size.
+    return str[i];
 }
 
 
@@ -150,7 +187,12 @@ int main()
     cout << "d3 = "; d3.show_dist(); cout << endl;
     cout << "d4 = "; d4.show_dist(); cout << endl;
 
+    // += operator (arithmetic add assign)
+    d4 += d3;
+    cout << "d4 = "; d4.show_dist(); cout << endl;
+
     // < operator (compare English distances)
+    cout << endl;
     if (d1 < d4)
     {   cout << "dist1 < dist4" << endl; }
     else
@@ -161,9 +203,29 @@ int main()
     String s2 = "all!";
     String s3;
     s3 = s1 + s2;           // concatinate two C-strings
-    cout << endl;
-    s3.display();
+    cout << endl; s3.display(); cout << endl;
+
+    // == operator (compare C-strings)
+    String s4 = "Hi", s5;
+    cout << "\nEnter string: ";
+    s5.get_str();
+    if(s4 == s5)
+    {   cout << "You entered \"Hi\"." << endl; }
+    else
+    {   cout << "You did not enter \"Hi\"." << endl; }
     
+    // [] operator (safe accessing)
+    char ch = s1[0];    // right-value
+    cout << "s1[0] = " << ch << endl;
+
+    s1[1] = 'a';        // left-value
+    s1.display();
+    cout << endl;
+
+    ch = s1[100];       // accessing index out of SIZE. 
+                        // (will throw an error messgae then exit without actually accessing)
+    s1[100] = 'a';
+
     
     return 0;
 }

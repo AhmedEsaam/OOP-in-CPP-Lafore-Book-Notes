72b18b6f34ed338c1f86bc5a5f3f3b03cf3ff1f8
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-10-06 00:53:36 +0300

    Adding object I/O, file ptrs,& file error handling

--- OOP_in_CPP/12_streams_and_files.cpp
+++ OOP_in_CPP/12_streams_and_files.cpp
@@ -4,6 +4,7 @@
 // #include <strstream>
 #include <string>
 #include <cstdlib>
+#include <process.h>        // for exit()
 
 using namespace std;
 
@@ -184,6 +185,28 @@ int isFeet(string str)
 */
 
 
+/// ♦ Object I/O ♦ ///////////////////////////////////////////////////////////
+// How user-defined objects can be written to and read from disk?
+
+class Person                                
+{
+private:
+    char name[40];
+    short age;
+public:
+    void getData()
+    {   
+        cout << "Enter name: "; cin >> name; 
+        cout << "Enter age: ";  cin >> age; 
+    }
+    void showData() const
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+    
+
 
 int main()
 {
@@ -322,9 +345,9 @@ int main()
         peek(ch)                        // Read one character, leave it in stream.
         count = gcount()                // Return number of characters read by a (immediately preceding) call to get(), getline(), or read().
         read(str, MAX)                  // For files—extract up to MAX characters into str, until EOF.
-        seekg()                         // Set distance (in bytes) of file pointer from start of file.
-        seekg(pos, seek_dir)            // Set distance (in bytes) of file pointer from specified place in file. seek_dir can be ios::beg, ios::cur, ios::end.
-        pos = tellg(pos)                // Return position (in bytes) of file pointer from start of file.
+        seekg(position)                 // Set distance (in bytes) of file pointer from start of file.
+        seekg(position, seek_dir)       // Set distance (in bytes) of file pointer from specified place in file. seek_dir can be ios::beg, ios::cur, ios::end.
+        pos = tellg()                   // Return position (in bytes) of file pointer from start of file.
     */
     
     
@@ -397,15 +420,15 @@ int main()
     // The stream error-status flags constitute an ios enum member that reports errors that occurred in an input or output operation.
     
     /* Error-Status Flags:________________________________________
-        goodbit             // No errors (no flags set, value = 0)
-        eofbit              // Reached end of file
-        failbit             // Operation failed (user error, premature EOF)
-        badbit              // Invalid operation (no associated streambuf)
+        goodbit             // 0: No errors (no flags set, value = 0)
+        eofbit              // 2: Reached end of file (Note: it's only set when you attempt to read or write NOT when the stream objects reaches the EOF in a previous operation)
+        failbit             // 4: Operation failed (user error, premature EOF)
+        badbit              // 1: Invalid operation (no associated streambuf)
         hardfail            // Unrecoverable error
     */
     
     /* Functions for Error-Status Flags:__________________________
-        int = eof();        // Returns true if EOF flag set
+        int = eof();        // Returns true if EOF flag set (Note: it's only set when you attempt to read or write NOT when the stream objects reaches the EOF in a previous operation)
         int = fail();       // Returns true if failbit or badbit or hardfail flag set
         int = bad();        // Returns true if badbit or hardfail flag set
         int = good();       // Returns true if everything OK; no flags set
@@ -582,6 +605,7 @@ int main()
     //  → If everything is going well, the object returns a 'nonzero' value.
 
 
+
     /// ♦ Character I/O ♦ ///////////////////////////////////////////////////////////
     // ► put(char) for writing:
     string str =    "Time is a great teacher, but unfortunately "
@@ -662,13 +686,254 @@ int main()
             { cerr << "Data is incorrect\n";    return 1; }
     
     cout << "Data is correct\n";
+    cout << endl;
+    
+
+
+    /// ♦ Object I/O ♦ ///////////////////////////////////////////////////////////
+    // To store objects' data into disk and read it.
+
+    // When writing an object, we generally want to use binary mode
+    // → This writes the same bit configuration to disk that was stored in memory.
+    // → And ensures that numerical data contained in objects is handled properly.
+    cin.setf(ios::skipws);
+    
+    Person per, _per;
+    per.getData();
+
+    // ► Write (store object):
+    ofstream outfile_Person("outfiles/person.dat", ios::binary);
+    outfile_Person.write(reinterpret_cast<char*>(&per), sizeof(per));
+
+    outfile_Person.close();
+
+
+    // ► Read (extract object):
+    ifstream infile_Person("outfiles/person.dat", ios::binary);
+    infile_Person.read(reinterpret_cast<char*>(&_per), sizeof(_per));
+
+    _per.showData();
+    
+    /* Note:
+        • It doesn’t matter what member functions you use, since they are not written to disk along with the object’s data.
+            → The data must have the same format, but inconsistencies in the member functions have no effect.
+        ◘ However, this is true only in simple classes that don’t use virtual functions.
+    */
+
+    /* CAUTIONS:
+        • Make sure a class used to read an object is identical to the class used to write it.
+            → With classes that use virtual funcions, objects derived from these classes include a mysterious number placed just before the object’s data in memory.
+            → This number is called the vtable pointer. It helps identify the object’s class when virtual functions are used.
+            → For every other class which have different member functions, this number changes as well.
+            → So, If you write an object of one class to a file, and then read it back into an object of a class that has identical data but a different member function, 
+                you’ll encounter big trouble if you try to use virtual functions on the object.
+
+        • You should not attempt to perform disk I/O with objects that have pointer data members.
+            → As the pointer values won’t be correct when the object is read back into a different place in memory.
+    */
+
+
+
+    /// ♦ I/O with Multiple Objects ♦ /////////////////////////////////////////////////////
+    char ch_;
+    Person per_;
+    fstream file_;                               // input/output file stream
+
+    // We create the file in one statement and open it in another, using the open() function.
+    //  → This is a useful approach in situations where the open may fail.
+    //      (You can create a stream object once, and then try repeatedly to open it, without the overhead of creating a new stream object each time.)
+    file_.open("outfiles/group.dat", ios::app | ios::out | ios::in | ios::binary);
+
+    /* ■ Mode Bits:
+        • (Mode specifiers or flags). They are defined in 'ios'
+        • Specify various aspects of how a stream object will be opened.
+        • The vertical bars (Bit-wise OR) between the flags cause the bits representing these flags to be logically combined into a single integer, 
+            → so that several flags can apply simultaneously
+
+        TABLE: Mode Bits for the open() Function:
+        _________________________________________________________________________________________________
+        Mode Bit        Result
+        _________________________________________________________________________________________________
+        in              Open for reading (default for ifstream)
+        out             Open for writing (default for ofstream) - but doesn't remove the old contents (So the new contents will overlay what's already there).
+        ate             Start reading or writing at end of file (AT End)
+        app             Start writing at end of file (APPend) (preserve whatever was in the file before)
+        trunc           Truncate file to zero length if it exists (TRUNCate)
+        nocreate        (Do not create the file if not existed) 
+                        - Error when opening if file does not already exist.
+        noreplace       (Do not replace file when writing if the file existed except if you append using 'ios::app' or 'ios::ate')
+                        - Error when opening for output if file already exists, unless ate or app is set.
+        binary          Open file in binary (not text) mode
+    */  
+
+    // ► Write:
+    do
+    {
+        cout << "\nEnter person's data:\n";
+        per_.getData();
+
+        file_.write(reinterpret_cast<char*>(&per_), sizeof(per_));
+        cout << "Enter another person (y/n)? ";
+        cin >> ch_;
+    } while (ch_ == 'y');
+
+    file_.seekg(0);                              // reset 'get' pointer to start-of-file
+
+
+    // ► Read:
+    // file_.read(reinterpret_cast<char*>(&per_), sizeof(per_));
+    // while(!file_.eof())
+    // {
+    //     cout << "\nPerson:\n";
+    //     per_.showData();
+    //     file_.read(reinterpret_cast<char*>(&per_), sizeof(per_));
+    // }
+    // cout << endl;
+    
+    while(file_.read(reinterpret_cast<char*>(&per_), sizeof(per_)))
+    {   
+        cout << "\nPerson:\n";
+        per_.showData();
+    }
+    cout << endl;
+
+
+
+    // file.close();
+    
+    /// ♦ File Pointers ♦ /////////////////////////////////////////////////////
+    /*
+        • They are not pointers used as address variables.
+        • They are used as a means of referring to a specific location in a file.
+            → They specify the byte number in the file where writing or reading will take place
+
+        • Each file object has associated with it two integer values:
+            ► 'get' pointer (current get position) - for reading
+                seekg(position)                 // Set distance (in bytes) of file pointer from start of file.
+                seekg(position, seek_dir)       // Set offset (in bytes) of file pointer from specified location in file. seek_dir can be ios::beg, ios::cur, ios::end.
+                pos = tellg()                   // Return position (in bytes) of file pointer from start of file.
+
+            ► 'put' pointer (current put position) - for writing
+                seekp(position)                 // Set distance (in bytes) of file pointer from start of file. (Note: seekp() has no effect if the ios::app is used; then every write goes at the end of the file.)
+                seekp(position, seek_dir)       // Set offset (in bytes) of file pointer from specified location in file. seek_dir can be ios::beg, ios::cur, ios::end.
+                pos = tellp()                   // Return position of file pointer, in bytes.
+
+                ex. seekp(-10, ios::end);
+                // will set the 'put' pointer to 10 bytes before the end of the file.
+
+        • The previous functions take one argument, which represents the absolute position in the file in bytes.
+            (The start of the file is byte 0.)
+        • There are times when you must take control of the file pointers yourself 
+            so that you can read from and write to an arbitrary location in the file.
+    */
+    
+    /// EXAMPLE: get number of objects in the file, and show specific object: - - - - - - - - - - -
+    Person pers;
+    ifstream in_file;
+    in_file.open("outfiles/group.dat", ios::in | ios::binary);
+
+    // Calculate number of Person objects in file:
+    in_file.seekg(0, ios::end);                 // go to 0 bytes from end 
+    int endposition = in_file.tellg();          // get current position (which is the end of file)
+    int total = endposition / sizeof(Person);
+    cout << "\nThere are " << total << " persons in file" << endl;
+
+    // Show specific Person object:
+    int num, position;
+    do
+    {
+        cout << "\nEnter person number: ";      // starting from 1
+        cin >> num;
+        position = (num - 1) * sizeof(Person);
+        in_file.seekg(position);
+        in_file.read(reinterpret_cast<char*>(&pers), sizeof(pers));
+        pers.showData();
+
+        cout << "Enter another (y/n)? ";
+        cin >> ch_;
+    } while (ch_ == 'y');
+    
+    
+    
+    /// ♦ Error Handling in File I/O ♦ ////////////////////////////////////////////////
+
+    /// ■ By checking the return value from the stream object itself to determine its error status.
+    //      → it returns a pointer value if everything went well, but 0 if it didn’t.
+
+    const int MAX_ = 1000;
+    int buff_[MAX_];
+
+    // fill buffer with data
+    for (int i = 0; i < MAX_; i++)              
+        buff_[i] = i;
+    
+    // ► writing an array to a file
+    ofstream os_;
+    os_.open("outfiles/a:edata.dat", ios::trunc | ios::binary);
+    if(!os_)
+        { cerr << "\nCould not open output file";   exit(1); }
+    
+    cout << "\nWriting...";
+    os_.write(reinterpret_cast<char*>(buff_), MAX_ * sizeof(int));
+    if(!os_)
+        { cerr << "\nCould not write to file";   exit(1); }
+    os_.close();
+
+
+    // clear buffer
+    for (int i = 0; i < MAX_; i++)
+        buff_[i] = 0;
+
+
+    // ► reading an array from a file
+    ifstream is_;
+    is_.open("outfiles/a:edata.dat", ios::binary);
+    if(!is_)
+        { cerr << "\nCould not open input file";   exit(1); }
+    
+    cout << "\nReading...";
+    is_.read(reinterpret_cast<char*>(buff_), MAX_ * sizeof(int));
+    if(!is_)
+        { cerr << "\nCould not read from file";   exit(1); }
+    is_.close();
+
+
+    // check data
+    for (int i = 0; i < MAX_; i++)
+        if(buff_[i] != i)
+            { cerr << "\nData is incorrect\n";  exit(1); }
+
+    cout << "\nData is correct\n";
+
+
+    // -----------------------------------------------------------------------------------------------------    
+    /// ■ Using the 'ios' error-status flags to find out more specific information about a file I/O error:
+    ifstream file;
+    file.open("outfiles/a:test.dat");
+
+    if(!file)
+        cout << "\nCan't open file";
+    else
+        cout << "\nFile opened successfully";
+
+    cout << "\nfile = "         << &file;
+    cout << "\nError state = "  << file.rdstate();      // returns 4 : returns the flag that is set to 1, here (failbit) which indicates that the file doesn’t exist.
+    cout << "\ngood() = "       << file.good();         // returns 0 : the good bit is set to 1 (true) only when no bits are set, so it returns 0 (false)
+    cout << "\neof() = "        << file.eof();          // returns 0 : we're not at EOF (end-of-file)
+    cout << "\nfail() = "       << file.fail();         // returns a non-zero value : since an error occured; operation failed (if 'failbit' or 'badbit' or 'hardfail' flag set)
+    cout << "\nbad() = "        << file.bad();          // returns 0 : it only return 1 if 'badbit' or 'hardfail' flag set
+    cout << endl;
+    file.close();
+
+    // In a serious program, some or all of these functions should be used after every I/O operation to ensure that things went as expected.
     
     
 
-    /// ♦ Object I/O ♦ ///////////////////////////////////////////////////////////
+    /// ♦ File I/O with Member Functions ♦ ////////////////////////////////////////////////
+
+        
     
     
-    
-    
+        
     return 0;
 }
